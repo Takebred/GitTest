eb748c322976380c5a49f82fdfc80c0ebbd70ca3
Takebred <bryantagatio@gmail.com>
2023-04-01 07:08:56 +0200

    Fin exercice 1 TP2Java

--- TP2Java/src/HelloUniverse.java
+++ TP2Java/src/HelloUniverse.java
@@ -1,5 +1,15 @@
 public class HelloUniverse {
     public static void main(String[] args) {
-
+//        exo 1
+        int annee = 2003, nbrePlanete;
+        String phrase = "En %d, les planètes du système solaire étaient au nombre de : %d\n";
+        if (annee < 2006) {
+            nbrePlanete = 9;
+            System.out.printf(phrase,annee,nbrePlanete);
+        }
+        if (annee >= 2006) {
+            nbrePlanete = 8;
+            System.out.printf(phrase,annee,nbrePlanete);
+        }
     }
 }
